d477d69b7693b2f0db044509a48ff24f5752ff38
memymini <minhi@gmail.com>
2024-03-13 12:52:05 +0900

    학과 추가

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ student_id = input("학번을 입력하세요: ")
 # 이름 입력
 student_name = input("이름을 입력하세요: ")
 
+# 학과 입력
+student_major = input("학과를 입력하세요: ")
+
 # 결과 출력
 print("입력한 학번:", student_id)
 print("입력한 이름:", student_name)
+print("입력한 학과:", student_major)
